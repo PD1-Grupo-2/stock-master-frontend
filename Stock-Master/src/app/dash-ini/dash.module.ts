daf8b18b16a02cb88b8895736b5477d7871d5a68
Marcelo Monteiro <102108594+Marcelo708@users.noreply.github.com>
2024-10-27 19:25:35 -0300

    Add files via upload

---
+++ Stock-Master/src/app/dash-ini/dash.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './dash.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
