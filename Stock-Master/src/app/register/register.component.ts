84922f0b9685fb3dc6197710be0ada7de7be7d21
Emilio de Oliveira <emilio.oliveira@safelight.info>
2024-10-27 13:24:38 -0300

    Ajuste do register.component e adiciona o logo a tela de login.

--- Stock-Master/src/app/register/register.component.ts
+++ Stock-Master/src/app/register/register.component.ts
@@ -1,36 +1,38 @@
 import { UserService } from '../services/user.service';
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-
 export class RegisterComponent {
-    username: string = '';
-    email: string = '';
-    password: string = '';
-    confirmPassword: string = ''
+  username: string = '';
+  email: string = '';
+  password: string = '';
+  confirmPassword: string = '';
 
-};
+  constructor(private userService: UserService) {}
 
-constructor(private userService: UserService) {}
-
-onRegister() {
-  if(this.user.password != this.user.confirmPassword) {
-    console.error('Senhas não coincidem');
-    return;
-  }
-};
-
-  this.userService.register(this.user).subscribe({
-    next: (response:any) => {
-      console.log('Usuário cadastrado com sucesso', response);
-    },
-    error: (err : any) => {
-      console.error('Erro ao cadastrar usuário', err);
+  onRegister() {
+    if (this.password !== this.confirmPassword) {
+      console.error('Senhas não coincidem');
+      return;
     }
-  })
+
+    const user = {
+      username: this.username,
+      email: this.email,
+      password: this.password
+    };
+
+    this.userService.register(user).subscribe({
+      next: (response: any) => {
+        console.log('Usuário cadastrado com sucesso', response);
+      },
+      error: (err: any) => {
+        console.error('Erro ao cadastrar usuário', err);
+      }
+    });
+  }
 }
