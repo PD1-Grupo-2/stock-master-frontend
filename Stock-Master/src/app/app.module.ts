cd140a913e2d713c93c9e778dc9eeb188d232817
Emilio de Oliveira <emilio.oliveira@safelight.info>
2024-11-27 09:48:54 -0300

    Adiciona páginas de clientes, estoque e fornecedores ao SPA

--- Stock-Master/src/app/app.module.ts
+++ Stock-Master/src/app/app.module.ts
@@ -6,10 +6,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { RegisterSupplierComponent } from './suppliers/register/register-supplier/register-supplier.component';
+import { RegisterSupplierComponent } from './components/suppliers/register/register-supplier/register-supplier.component';
 import { HomeComponent } from './home/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BottomNavbarComponent } from './components/bottom-navbar/bottom-navbar.component';
+import { ClientListComponent } from './components/clients/client-list/client-list.component';
+import { RegisterClientComponent } from './components/clients/register-client/register-client.component';
+import { ProductListComponent } from './components/products/product-list/product-list.component';
+import { RegisterProductComponent } from './components/products/register-product/register-product.component';
+import { SuppliersListComponent } from './components/suppliers/suppliers-list/suppliers-list.component';
+import { StockListComponent } from './components/stock/stock-list/stock-list.component';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
 
 @NgModule({
   declarations: [
@@ -19,13 +30,25 @@ import { BottomNavbarComponent } from './components/bottom-navbar/bottom-navbar.
     RegisterSupplierComponent,
     HomeComponent,
     HeaderComponent,
-    BottomNavbarComponent
+    BottomNavbarComponent,
+    ClientListComponent,
+    RegisterClientComponent,
+    ProductListComponent,
+    RegisterProductComponent,
+    SuppliersListComponent,
+    StockListComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatTabsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Stock-Master/src/app/components/stock/stock-list/stock-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-list',
+  templateUrl: './stock-list.component.html',
+  styleUrls: ['./stock-list.component.css']
+})
+export class StockListComponent implements OnInit {
+  entradaProducts = [
+    { name: 'Produto 1', quantity: 100, price: 10.00 },
+    { name: 'Produto 2', quantity: 50, price: 20.00 },
+    { name: 'Produto 3', quantity: 200, price: 5.00 },
+    // Add more products as needed
+  ];
+
+  saidaProducts = [
+    { name: 'Produto 4', quantity: 30, price: 15.00 },
+    { name: 'Produto 5', quantity: 20, price: 25.00 },
+    { name: 'Produto 6', quantity: 10, price: 30.00 },
+    // Add more products as needed
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
